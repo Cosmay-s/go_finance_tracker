f63db04e27236d1ac3a90bf42c5e2611145ff40c
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-12 21:07:12 +0300

    переменная окружения добавлена

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"github.com/cosmay-s/go_finance_tracker/config"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	cfg := config.LoadConfig()
 	app := echo.New()
 
 	app.GET("/", func(c echo.Context) error {
-		return c.String(200, "Приложение запущено")
+		return c.String(200, "Приложение запущено"+cfg.Port)
 	})
 
-	app.Logger.Fatal(app.Start(":8080"))
+	app.Logger.Fatal(app.Start(":" + cfg.Port))
 
 }
