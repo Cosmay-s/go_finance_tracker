0cf16e0cbf11bf9cd737f2b092d118e6b3883f32
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-13 18:09:57 +0300

    база создана и подключена

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/cosmay-s/go_finance_tracker/config"
+	"github.com/cosmay-s/go_finance_tracker/internal/database"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+
+	db, err := database.InitDB(cfg)
+	if err != nil {
+		log.Fatalf("Ошибка подключение к бд: %v", err)
+	}
 	app := echo.New()
+
+	_ = db
 
 	app.GET("/", func(c echo.Context) error {
 		return c.String(200, "Приложение запущено"+cfg.Port)
