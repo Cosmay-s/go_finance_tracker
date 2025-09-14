6b3f4457fcd105128a714d32a56a3fb5ecce091e
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-14 20:58:57 +0300

    ап

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID           uint      `gorm:"primaryKey"`
-	Email        string    `gorm:"unique;not null"`
-	PasswordHash string    `gorm:"not null"`
+	Email        string    `gorm:"unique;not null;type:varchar(255)"`
+	PasswordHash string    `gorm:"not null;type:varchar(255)"`
 	CreatedAt    time.Time `gorm:"autoCreateTime"`
 }
