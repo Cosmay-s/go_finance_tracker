4b082b16cce6119fff7df320260f9ace7ee3630b
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-17 00:39:04 +0300

    ручка регистраций

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "github.com/cosmay-s/go_finance_tracker/internal/services"
+import (
+	"net/http"
+
+	"github.com/cosmay-s/go_finance_tracker/internal/services"
+	"github.com/labstack/echo/v4"
+)
 
 type AuthHandles struct {
 	authService *services.AuthService
@@ -22,4 +27,33 @@ type LoginRequest struct {
 
 type LoginResponse struct {
 	Token string `json:"token"`
+}
+
+func (h *AuthHandles) Register(c echo.Context) error {
+	var req RegisterRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"ошибка": err.Error(),
+		})
+	}
+
+	if err := c.Validate(req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"ошибка": err.Error(),
+		})
+	}
+	err := h.authService.Register(services.RegisterRequest{
+		Email:    req.Email,
+		Password: req.Password,
+	})
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"ошибка": err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusCreated, map[string]string{
+		"уведомление": "Пользователь создан",
+	})
 }
