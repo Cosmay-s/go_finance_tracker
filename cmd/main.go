f9887f8aa3a404f9f5170d609d23f8bc8bdac827
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-17 01:07:24 +0300

    main update + moddleware

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,11 @@ import (
 
 	"github.com/cosmay-s/go_finance_tracker/config"
 	"github.com/cosmay-s/go_finance_tracker/internal/database"
+	"github.com/cosmay-s/go_finance_tracker/internal/handlers"
+	"github.com/cosmay-s/go_finance_tracker/internal/models"
+	"github.com/cosmay-s/go_finance_tracker/internal/repositories"
+	"github.com/cosmay-s/go_finance_tracker/internal/services"
+	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
 )
 
@@ -15,13 +20,29 @@ func main() {
 	if err != nil {
 		log.Fatalf("Ошибка подключение к бд: %v", err)
 	}
+
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatalf("Ошибка миграции: %v", err)
+	}
+
 	app := echo.New()
 
-	_ = db
+	app.Use(middleware.Logger())
+	app.Use(middleware.Recover())
+	app.Use(middleware.CORS())
 
+	userRepo := repositories.NewUserRepository(db)
+	authSevice := services.NewAuthService(userRepo, cfg.JwtSecret)
+	authHandler := handlers.NewAuthHandler(authSevice)
+
+	//ручки
 	app.GET("/", func(c echo.Context) error {
 		return c.String(200, "Приложение запущено"+cfg.Port)
 	})
+
+	app.POST("/register", authHandler.Register)
+	app.POST("/login", authHandler.Login)
 
 	app.Logger.Fatal(app.Start(":" + cfg.Port))
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.13.4
 	golang.org/x/crypto v0.42.0
 	gorm.io/driver/postgres v1.6.0
@@ -12,6 +13,7 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
