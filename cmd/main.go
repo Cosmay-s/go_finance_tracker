aeec2bd1917b699504d525a32c72f6f86c49a775
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-17 01:18:32 +0300

    validator add

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,18 @@ import (
 	"github.com/cosmay-s/go_finance_tracker/internal/models"
 	"github.com/cosmay-s/go_finance_tracker/internal/repositories"
 	"github.com/cosmay-s/go_finance_tracker/internal/services"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
 )
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
 
 func main() {
 	cfg := config.LoadConfig()
@@ -28,6 +37,8 @@ func main() {
 
 	app := echo.New()
 
+	app.Validator = &CustomValidator{validator: validator.New()}
+
 	app.Use(middleware.Logger())
 	app.Use(middleware.Recover())
 	app.Use(middleware.CORS())
@@ -38,7 +49,7 @@ func main() {
 
 	//ручки
 	app.GET("/", func(c echo.Context) error {
-		return c.String(200, "Приложение запущено"+cfg.Port)
+		return c.String(200, "Приложение запущено - порт:"+cfg.Port)
 	})
 
 	app.POST("/register", authHandler.Register)
